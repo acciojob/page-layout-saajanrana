dcfd196bea8ba2d8fc41341b054b31fc8f5fe11a
saajanrana <saajanrana001@gmail.com>
2023-07-11 16:41:06 +0530

    submitting code-Tue Jul 11 2023 16:41:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App = (props) => {
+    return (
+        <div>
+            <h1>{props.header}</h1>
+            <p>{props.children}</p>
+            <h3>{props.footer}</h3>
+        </div>
+    )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App header="Welcome to my website." footer="© 2023 My Website. All rights reserved."><p>This is the content of my website.</p></App> , document.getElementById("root"));
